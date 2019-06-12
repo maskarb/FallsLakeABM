c79973cdf5c26e904ccad452043cf1dd1db5da78
michael skarbek <maskarb@gmail.com>
2019-06-12 18:45:53 -0400

    working policy. drought stages enabled and recision targets working.

--- src/PolicyMaker.java
+++ src/PolicyMaker.java
@@ -5,7 +5,6 @@ import sim.engine.Steppable;
 /** @author amashha */
 public class PolicyMaker implements Steppable {
   static final long serialVersionUID = 1L;
-
   private double elevation;
   private Reservoir reservoir;
   public int stage;
@@ -13,7 +12,6 @@ public class PolicyMaker implements Steppable {
   private double storage;
   // private double waterQualityStorage = 61322; // acre-feet
   private double waterSupplyStorage = 45000; // acre-feet
-  private double recision = 0;
   private double[] reductionPercentages;
   private HashMap<Integer, double[]> droughtStages;
   private HashMap<Integer, double[]> recisionStages;
@@ -53,12 +51,7 @@ public class PolicyMaker implements Steppable {
     int month = time % 12 + 1;
     // Household.setConservationStage(stage);
     double[] droughtPercent = droughtStages.get(month);
-    System.out.println(month);
-
-    // if drought state from previous month is greater than 0, get the recision stage
-    if (stage > 0) {
-      recision = recisionStages.get(month)[stage - 1];
-    }
+    double[] recisionPercent = recisionStages.get(month);
 
     if (isDroughtRestriction) {
       this.elevation = getelevation();
@@ -67,16 +60,17 @@ public class PolicyMaker implements Steppable {
       double baseStorage = ((FallsLake) reservoir).getLowestStorage();
 
       double wssp = 0.423 * (storage - baseStorage);
-      if (wssp >= (reductionPercentages[0] * waterSupplyStorage)
-          && wssp >= (waterSupplyStorage * recision)) {
+      if (wssp >= (droughtPercent[0] * waterSupplyStorage)
+          && wssp >= (recisionPercent[0] * waterSupplyStorage)) {
         stage = 0;
       } else if (wssp >= (droughtPercent[1] * waterSupplyStorage)
-          && wssp >= (waterSupplyStorage * recision)) {
+          && wssp >= (recisionPercent[1] * waterSupplyStorage)) {
         stage = 1;
       } else if (wssp >= (droughtPercent[2] * waterSupplyStorage)
-          && wssp >= (waterSupplyStorage * recision)) {
+          && wssp >= (recisionPercent[2] * waterSupplyStorage)) {
         stage = 2;
-      } else if (wssp >= (0 * waterSupplyStorage) && wssp >= (waterSupplyStorage * recision)) {
+      } else if (wssp >= (0 * waterSupplyStorage) 
+          && wssp >= (0 * waterSupplyStorage)) {
         stage = 3;
       } else {
         stage = 4;
@@ -99,7 +93,6 @@ public class PolicyMaker implements Steppable {
           reductionFactor = (25 / 65.0);
           break;
       }
-      System.out.println(stage);
       Household.setConservationFactor(reductionFactor);
     }
   }
